11339130792ef4e60436de0b44ba0e243057bc90
Roman Steiner <roman@toastlab.ch>
2018-06-05 17:16:31 +0200

    complex example

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const lorem = ["Lorem ipsum", "dolor sit", "amet", "consectetur", "adipiscing elit"]
+
 if (window.commaship) {
   commaship.register('World Greeter', [
     {
@@ -5,6 +7,22 @@ if (window.commaship) {
       action: function() { alert('Hello World!'); },
       label: 'Greet planet',
       description: 'This salutes the whole world at once! Incredible technology ahead!',
+    },
+    {
+      id: 'complex-async-example',
+      label: 'Complex async example',
+      description: 'Lookup the number of stars in any getkirby repo',
+      action: function() {
+        return new Promise(async (resolve, reject) => {
+          const repos = await fetch('https://api.github.com/orgs/getkirby/repos').then(r => r.json())
+          const newOptions = repos.map(r => ({
+            label: r.name,
+            description: r.description,
+            action: () => alert(`${r.name} has ${r.stargazers_count} stars.`)
+          }))
+          resolve(newOptions)
+        })
+      }
     }
   ])
 } else {
